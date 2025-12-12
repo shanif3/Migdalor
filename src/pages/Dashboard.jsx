36533d0eef77091186fb3aa9e0359bbffbde6d18
Shani Finkelstein <shaninn123@gmail.com>
2025-12-12 10:41:28 +0000

    Manual code change

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -152,7 +152,7 @@ export default function Dashboard() {
                 onClick={() => setShowQueueModal(true)} className="bg-background text-blue-600 px-4 py-2 text-sm font-medium rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border shadow-sm hover:text-accent-foreground h-9 border-blue-200">
 
 
-                הוסף לתור
+                הוסף בקשה מיוחדת
                 <Plus className="w-4 h-4 mr-2" />
 
               </Button>
