b8aa66508f16411c4d5b88c844a86f3ddf208250
Shani Finkelstein <shaninn123@gmail.com>
2025-12-17 16:54:26 +0000

    Manual code change

--- src/components/queue/AddToQueueModal.jsx
+++ src/components/queue/AddToQueueModal.jsx
@@ -49,13 +49,14 @@ const [endTime, setEndTime] = useState('');
 <Dialog open={open} onOpenChange={onClose}>
       <DialogContent dir="rtl" className="text-right">
 
-        <DialogHeader className="text-right">
-<DialogTitle className="flex items-center justify-end gap-2 text-right w-full">
-  בקשה מיוחדת למפתח
-  <div className="p-2 bg-blue-100 rounded-lg">
-    <Clock className="w-5 h-5 text-blue-600" />
-  </div>
-</DialogTitle>
+<DialogHeader className="text-right">
+  <DialogTitle className="flex items-center justify-end gap-2 w-full">
+    בקשה מיוחדת למפתח
+    <div className="p-2 bg-blue-100 rounded-lg">
+      <Clock className="w-5 h-5 text-blue-600" />
+    </div>
+  </DialogTitle>
+
 
 
           <DialogDescription className="text-right">
