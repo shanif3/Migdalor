69ddddc377b6c1b12d588a9e8eb3d23a97a8dd5e
Shani Finkelstein <shaninn123@gmail.com>
2025-12-17 17:01:58 +0000

    File changes

--- src/components/queue/AddToQueueModal.jsx
+++ src/components/queue/AddToQueueModal.jsx
@@ -50,12 +50,12 @@ const [endTime, setEndTime] = useState('');
       <DialogContent dir="rtl" className="text-right">
 
 <DialogHeader className="text-right">
-<DialogTitle className="w-full !text-right">
-  <div className="flex w-full items-center justify-end gap-2">
+<DialogTitle className="w-full text-right">
+  <div className="flex w-full items-center gap-2">
     <div className="p-2 bg-blue-100 rounded-lg">
       <Clock className="w-5 h-5 text-blue-600" />
     </div>
-    בקשה מיוחדת למפתח
+    <span className="text-right">בקשה מיוחדת למפתח</span>
   </div>
 </DialogTitle>
 
