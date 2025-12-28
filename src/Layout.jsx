607bbe2d071c78892f53c701656c94b431260143
Shani Finkelstein <shaninn123@gmail.com>
2025-12-28 23:58:29 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -61,6 +61,7 @@ export default function Layout({ children, currentPageName }) {
 
   const userNavItems = [
     { name: 'לוח בקרה', icon: LayoutDashboard, page: 'Dashboard', tooltip: 'לוח בקרה' },
+    { name: 'תמונת מצב', icon: Calendar, page: 'DailyOverview', tooltip: 'תמונת מצב' },
     { name: 'לוח הזמנים שלי', icon: Calendar, page: 'MySchedule', tooltip: 'לוח זמנים' },
     { name: 'מפתחות', icon: Key, page: 'ManageKeys', tooltip: 'מפתחות' },
     { name: 'אזור אישי', icon: Target, page: 'MyProfile', tooltip: 'אזור אישי' },
